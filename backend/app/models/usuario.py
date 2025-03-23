0aadb0947ab96e8bc1c9d166726bd450825c9a8b
pabcascom <pabcascom@alum.us.es>
2025-03-23 11:13:21 +0100

    fix: nombres concordantes
    No comments needed

--- backend/app/models/usuario.py
+++ backend/app/models/usuario.py
@@ -1,19 +1,36 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app.extensions import db
+
 
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'  # Nombre de la tabla en la BD
+    __tablename__ = 'usuarios'  ## Nombre de la tabla en la BD
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    correo = db.Column(db.String(100), unique=True, nullable=False)
-    contraseña = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     
-    def __init__(self, nombre, correo, contraseña):
-        self.nombre = nombre
-        self.correo = correo
-        self.contraseña = contraseña
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
         
+    def hash_password(self):
+        self.password = generate_password_hash(self.password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
         
     @classmethod
     def find_by_username_or_email(cls, username, email):
         return cls.query.filter((cls.username == username) | (cls.email == email)).first()
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
--- backend/data.sql
+++ backend/data.sql
@@ -1,7 +1,5 @@
--- Sample data for a pet clinic database
-
 -- Insert data into usuarios table
-INSERT INTO usuarios (id, nombre, correo, contraseña) VALUES
+INSERT IGNORE INTO usuarios (id, username, email, password) VALUES
 (1, 'Daniel', 'danbenhid@example.com', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
 (2, 'Fernando', 'ferdehur@example.com', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
 (3, 'Pablo', 'pabcascom@example.com', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK');
