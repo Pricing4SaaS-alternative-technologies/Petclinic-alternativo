c31807cac8f2f80947ae11d8f3eec20b5b882ba4
pabcascom <pabcascom@alum.us.es>
2025-03-23 19:52:47 +0100

    fix: data.sql actualizado con las contraseñas hasheadas
    #3

--- backend/data.sql
+++ backend/data.sql
@@ -1,5 +1,5 @@
 -- Insert data into usuarios table
 INSERT IGNORE INTO usuarios (id, username, email, password) VALUES
-(1, 'Daniel', 'danbenhid@example.com', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
-(2, 'Fernando', 'ferdehur@example.com', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
-(3, 'Pablo', 'pabcascom@example.com', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK');
+(1, 'Daniel', 'danbenhid@example.com', 'scrypt:32768:8:1$uh1IGF90LTfiEraD$58161931e2e2f229cd8b59b2d63820a2c50000b35faf63a0b54aa3225c6d5f74e3dbd89495fd0d011f5aa3a2cf7e89b4a22e24f923a6bd45074cefe125e2fb33'),
+(2, 'Fernando', 'ferdehur@example.com', 'scrypt:32768:8:1$uh1IGF90LTfiEraD$58161931e2e2f229cd8b59b2d63820a2c50000b35faf63a0b54aa3225c6d5f74e3dbd89495fd0d011f5aa3a2cf7e89b4a22e24f923a6bd45074cefe125e2fb33'),
+(3, 'Pablo', 'pabcascom@example.com', 'scrypt:32768:8:1$uh1IGF90LTfiEraD$58161931e2e2f229cd8b59b2d63820a2c50000b35faf63a0b54aa3225c6d5f74e3dbd89495fd0d011f5aa3a2cf7e89b4a22e24f923a6bd45074cefe125e2fb33');
