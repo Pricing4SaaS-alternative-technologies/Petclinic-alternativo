f92362d7ac067eae043afbf24128c7cb9cc34939
pabcascom <pabcascom@alum.us.es>
2025-04-16 12:43:01 +0200

    feat: entidades clinica, consulta, habitacionHotel, mascota

---
+++ backend/app/models/habitacionHotel.py
@@ -0,0 +1,33 @@
+from enum import Enum as PyEnum
+from app.extensions import db
+
+class PetType(PyEnum):
+    CAT = "CAT"
+    DOG = "DOG"
+    LIZARD = "LIZARD"
+    SNAKE = "SNAKE"
+    BIRD = "BIRD"
+    HAMSTER = "HAMSTER"
+    TURTLE = "TURTLE"
+
+
+class HabitacionHotel(db.Model):
+    __tablename__ = 'habitaciones_hotel'  ## Nombre de la tabla en la BD
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    size = db.Column(db.Integer, nullable=False, default=0)
+    type = db.Column(db.Enum(PetType), nullable=False) # NO SE SI AL SER NULLABLE FALSE HAY QUE PONER DEFAULT
+
+    
+    def __init__(self, size, type):
+        self.size = size
+        self.type = type
+        
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
---
+++ backend/app/models/mascota.py
@@ -0,0 +1,37 @@
+from enum import Enum as PyEnum
+from app.extensions import db
+
+class PetType(PyEnum):
+    CAT = "CAT"
+    DOG = "DOG"
+    LIZARD = "LIZARD"
+    SNAKE = "SNAKE"
+    BIRD = "BIRD"
+    HAMSTER = "HAMSTER"
+    TURTLE = "TURTLE"
+
+
+class Mascota(db.Model):
+    __tablename__ = 'mascotas'  ## Nombre de la tabla en la BD
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    birthDate = db.Column(db.Date, nullable=False)
+    adopted = db.Column(db.Boolean, nullable=False, default=False)
+    type = db.Column(db.Enum(PetType), nullable=False) # NO SE SI AL SER NULLABLE FALSE HAY QUE PONER DEFAULT
+
+    
+    def __init__(self, name, birthDate, adopted, type):
+        self.name = name
+        self.birthDate = birthDate
+        self.adopted = adopted
+        self.type = type
+        
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
