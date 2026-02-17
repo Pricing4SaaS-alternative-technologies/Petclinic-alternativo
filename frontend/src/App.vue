2a29579fac4e15ae10932acd8eb5d464b5a916f7
d4niel980 <danbenhid@alum.us.es>
2026-02-17 19:44:27 +0100

    feat: App.vue configured
    The space vue client has been configured in the App.vue file
    
    #78

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <router-view/>
-  </div>
+  <SpaceProvider :config="spaceConfig">
+    <div id="app">
+      <Navbar />
+      <router-view/>
+    </div>
+  </SpaceProvider>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import { SpaceProvider } from '@npm_team/space-vue-client'
 import '@/App.css'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    SpaceProvider
+  },
+  data () {
+    return {
+      spaceConfig: {
+        allowConnectionWithSpace: false
+      }
+    }
   }
 }
 </script>
